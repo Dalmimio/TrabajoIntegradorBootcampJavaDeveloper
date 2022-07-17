a3e0b095a06d6fa6fca518c53a8fb78b62210a11
Dalila <ilikeitati@gmail.com>
2022-07-17 00:29:37 -0300

    Agregando user y post

---
+++ Blogspring/src/main/java/com.dalmiromi.blogspring.model/Post.java
@@ -0,0 +1,77 @@
+import java.time.Instant;
+import java.lang.Long;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table
+
+public class Post{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    @NotBlank
+    @Column
+    private String title;
+    @Lob
+    @Column
+    @NotEmpty
+    private String content;
+    @Column
+    private Instant createdOn;
+    @Column
+    private Instant updateOn;
+    @Column
+    @NotBlank
+    private String userName;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public Instant getCreatedOn() {
+        return createdOn;
+    }
+    public void setCreatedOn(Instant createdOn) {
+        this.createdOn = createdOn;
+    }
+    public Instant getUpdateOn() {
+        return updateOn;
+    }
+    public void setUpdateOn(Instant updateOn) {
+        this.updateOn = updateOn;
+    }
+    public String getUsername() {
+        return userName;
+    }
+    public void setUsername(String username) {
+        this.userName = username;
+    }
+
+
+    
+
+
+}
